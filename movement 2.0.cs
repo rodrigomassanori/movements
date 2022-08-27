f2c4717549cf736d4b0dbcb96d3ba54441ac13b0
vitey-san <112221882+vitey-san@users.noreply.github.com>
2022-08-27 18:59:35 -0300

    Create movement 2.0.cs

---
+++ movement 2.0.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class movement2 : MonoBehaviour 
+{
+
+	CharacterController controller;
+    //velocidade e gravidade
+    public float Speed = 12f;
+   
+	public float gravity = -9.81f;
+    
+	
+	Vector3 velocity;
+  
+	public Transform groundCheck;
+    
+	public float groundDistance =0.4f;
+    
+	public LayerMask groundMask;
+    
+	bool isGrounded;
+     
+	Rigidbody m_Rigidbody;
+
+	
+	void Awake()
+	{
+		controller = GetComponent<CharacterController>();
+        
+		 m_Rigidbody = GetComponent <Rigidbody>();
+	}
+
+	void FixedUpdate() 
+ 	{ 
+         
+
+       if( Input.GetButton("jump"))
+			
+			{
+			m_Rigidbody.AddForce(transform.up * m_Thrust);
+
+			}
+  
+	}		
+		
+	
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		//controle
+        float x = Input.GetAxis("Vertical");
+        
+		float z = Input.GetAxis("Horizontal");
+        
+        Vector3  move = transform.right * x + transform.forward * z;
+		
+	
+        
+        controller.Move(move * Speed * Time.deltaTime);
+
+
+        velocity.y += gravity * Time.deltaTime;
+        
+		controller.Move(velocity * Time.deltaTime);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+    	if(isGrounded && velocity.y < 0)
+		{
+        	velocity.y = -2f;
+		}
+		
+
+		}
+}
